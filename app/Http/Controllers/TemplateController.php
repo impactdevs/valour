9656e15bcb1e87227e0e552676b1553ba6d03220
Dav0311 <ochwodavid03@gmail.com>
2024-06-20 14:16:01 +0300

    valour landing page

---
+++ app/Http/Controllers/TemplateController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TemplateController extends Controller
+{
+    //
+    public function home()
+    {
+        return view('layouts.index');
+    }
+    
+}
